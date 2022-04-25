b48d9920b3655242322356b20b7fa5b1975d4407
rabucejo <rogerhapay@gmail.com>
2022-04-25 11:40:12 +0800

    added textfield and header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import TodoForm from './components/TodoForm'
+import { Typography } from '@material-ui/core'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant='h5'>
+        <TodoForm/>
+      </Typography>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
