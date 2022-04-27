5e0aeaf378ed795c3633572c2c471c1d8e4efe04
rabucejo <rogerhapay@gmail.com>
2022-04-27 12:07:46 +0800

    change all files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css'
-import TodoForm from './components/TodoForm'
-import { Typography } from '@material-ui/core'
 
 function App() {
-  return (
-    <div>
-      <Typography component='h1' variant='h2'>
-        Todos
-      </Typography>
-      <TodoForm saveTodo={console.warn} />
-    </div>
-  )
+  return 
+  
 }
 
 export default App
