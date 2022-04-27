a9886d3e1a196bf87730f2132105563ad648c01f
rabucejo <rogerhapay@gmail.com>
2022-04-27 09:25:44 +0800

    add todolist file

---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './TodoForm.css'
+import { Button } from '@material-ui/core'
+import { useState } from 'react'
+
+const TodoForm = ({ saveTodo }) => {
+  const [value, setValue] = useState('')
+  return (
+    <>
+      <h1 className='todoheader'>To-Do List</h1>
+      <div className='form'>
+        <form
+          className='todoform'
+          onSubmit={(e) => {
+            e.preventDefault()
+            saveTodo(value)
+            setValue('')
+          }}
+        >
+          <input
+            className='todotextfield'
+            variant='outlined'
+            placeholder='Add Task'
+            margin='normal'
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value)
+            }}
+          ></input>
+          <div className='add-btn'>
+            <Button className='addBtn' variant='outlined' color='primary'>
+              Add
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default TodoForm
